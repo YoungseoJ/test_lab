fe13c8a145da76c8bdf144bd88c0ff591de5be8a
Youngseo Jang <144073552+YoungseoJ@users.noreply.github.com>
2023-09-05 15:09:24 +0900

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -17,19 +17,28 @@ export default function FeedbackForm() {
             </label>
         </p>
 
-          
+          <p>
         <label htmlFor="name">나는 하루에 '<input id="name" className={styles['form-field']} type="text" name="name" />분씩 걸을 것이다.</label>
-        
+          </p>
+
+          <p>
         <label htmlFor="name">'나는 내가 좋아하는 활동 중, '<input id="name" className={styles['form-field']} type="text" name="name" />'을(를) 더욱 자주, 더욱 활발하게 할 것이다.'</label>
+          </p>
 
+          <p>
         <label htmlFor="name">나는 신체활동을 습관화하기 위해 최선을 다할 것이다.</label>
+          </p>
 
+          <p>
         <label htmlFor="name">나는 이를 '<input id="name" className={styles['form-field']} type="text" name="name" />부터 반드시 실천할 것이다.</label>
+          </p>
 
+          <p>
         <label htmlFor="name">이름을 적어주세요.</label>
         <input id="name" className={styles['form-field']} type="text" name="name" />
+          </p>
           
-        <button className={styles.button} type="submit">Submit</button>
+        <button className={styles.button} type="submit">서약서 작성하기</button>
       </form>
   )
 }
