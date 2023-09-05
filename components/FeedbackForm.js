a5caf4eee85a096c684d174572aca54d44775899
Youngseo Jang <144073552+YoungseoJ@users.noreply.github.com>
2023-09-05 14:54:12 +0900

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -16,15 +16,19 @@ export default function FeedbackForm() {
             Don’t fill this out if you’re human: <input name="bot-field" />
             </label>
         </p>
-  
-        <label htmlFor="name">Name</label>
+
+          
+        <label htmlFor="name">나는 하루에 '<input id="name" className={styles['form-field']} type="text" name="name" />분씩 걸을 것이다.</label>
+        
+        <label htmlFor="name">'나는 내가 좋아하는 활동 중, '<input id="name" className={styles['form-field']} type="text" name="name" />'을(를) 더욱 자주, 더욱 활발하게 할 것이다.'</label>
+
+        <label htmlFor="name">나는 신체활동을 습관화하기 위해 최선을 다할 것이다.</label>
+
+        <label htmlFor="name">나는 이를 '<input id="name" className={styles['form-field']} type="text" name="name" />부터 반드시 실천할 것이다.</label>
+
+        <label htmlFor="name">이름을 적어주세요.</label>
         <input id="name" className={styles['form-field']} type="text" name="name" />
-
-        <label htmlFor="email">Email</label>
-        <input id="email" className={styles['form-field']} type="email" name="email" required />
-
-        <label htmlFor="feedback">What is your feedback?</label>
-        <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
+          
         <button className={styles.button} type="submit">Submit</button>
       </form>
   )
