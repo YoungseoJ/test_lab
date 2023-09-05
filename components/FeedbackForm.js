6f40b0a73be69c27825dc351d7babee7fd010b7c
Youngseo Jang <144073552+YoungseoJ@users.noreply.github.com>
2023-09-05 15:33:16 +0900

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -18,17 +18,18 @@ export default function FeedbackForm() {
         </p>
 
           <p>
-        <label htmlFor="name">나는 하루에 <input id="name" className={styles['form-field']} type="text" name="name" />분씩 걸을 것이다.</label>
+        <label htmlFor="name">나는 하루에 <input id="name" className={styles['form-field']} type="text" name="name" /> 분씩 걸을 것이다.</label>
           </p>
 
           <p>
-        <label htmlFor="name">나는 내가 좋아하는 활동 중, <input id="name" className={styles['form-field']} type="text" name="name" />을(를) 
+        <label htmlFor="name">나는 내가 좋아하는 활동 중, <input id="name" className={styles['form-field']} type="text" name="name" /> 을(를) 
             </p>
          <p>
 더욱 자주, 더욱 활발하게 할 것이다.</label>
           </p>
          
           <p> 
+               <label htmlFor="name"> </label>
           </p>
           
           <p>
@@ -36,11 +37,11 @@ export default function FeedbackForm() {
           </p>
 
           <p>
-        <label htmlFor="name">나는 이를 <input id="name" className={styles['form-field']} type="text" name="name" />부터 반드시 실천할 것이다.</label>
+        <label htmlFor="name">나는 이를 <input id="name" className={styles['form-field']} type="text" name="name" /> 부터 반드시 실천할 것이다.</label>
           </p>
 
           <p>
-        <label htmlFor="name">내 이름은 <input id="name" className={styles['form-field']} type="text" name="name" />이다.</label>
+        <label htmlFor="name">내 이름은 <input id="name" className={styles['form-field']} type="text" name="name" /> 이다.</label>
           </p>
 
           
