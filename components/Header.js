379bba8378ce6c0efe183f99c413441fa1dd9e40
Youngseo Jang <144073552+YoungseoJ@users.noreply.github.com>
2023-09-05 15:48:12 +0900

    Update Header.js
    그 링크 추가한거....

--- components/Header.js
+++ components/Header.js
@@ -11,6 +11,33 @@ export default function Header({ title }) {
       <p>
         목표와 이름을 작성하고 버튼을 누르면 나만의 실천 목표 서약서를 만들 수 있어요.😊✌️
       </p>
+
+
+
+
+    
+          <p>
+        Hi 👋! This template gives you a{" "}
+        <a href="https://nextjs.org/">Next.js</a> app with the scaffolding for{" "}
+        <a href="https://www.netlify.com/products/functions/">
+          Netlify Functions
+        </a>
+        , <a href="https://www.netlify.com/products/forms/">Forms</a>, and{" "}
+        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
+        aim was to give you the code you would need to hit the ground running
+        with a few fun features.
+      </p>
+
+      <p>
+        You can find the code for this project on GitHub at{" "}
+        <a href="https://github.com/netlify-templates/nextjs-toolbox">
+          https://github.com/netlify-templates/nextjs-toolbox
+        </a>
+        ! Happy coding!
+      </p>
+
+
+          
     </>
   );
 }
